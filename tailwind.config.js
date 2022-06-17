98f94c6fee8575e6f695e8af3b9384b90a3b7f32
ferrjohnpainagan <ferrjohnpainagan@gmail.com>
2022-06-17 18:34:16 +0800

    WIP: Login to bank feature

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins'],
+        workSans: ['Work Sans'],
+      },
+      colors: {
+        blue1: '#038BF4',
+        indigo1: '#474E6D',
+        defaultBg: '#F9F9F9',
+        cardStroke: '#E8E8E8',
+      },
+    },
   },
   plugins: [],
 }
