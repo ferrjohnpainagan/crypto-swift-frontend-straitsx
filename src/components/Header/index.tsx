d0efb850d84669609c930e1634537e2e193ee8f0
ferrjohnpainagan <ferrjohnpainagan@gmail.com>
2022-06-22 14:19:45 +0800

    Added navigation to login bank button
    Added navigation for home button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import Remit from './Remit'
 
 const Header = () => {
+  const navigate = useNavigate()
+  const handleHome = () => {
+    navigate('/bank-login')
+  }
   return (
     <div className="flex h-20 w-screen items-center border-b-gray-400 bg-white px-24 shadow-sm">
-      <div className="font-poppins text-xl font-extrabold">
+      <div
+        className="cursor-pointer font-poppins text-xl font-extrabold"
+        onClick={handleHome}
+      >
         <a className="text-blue1">Crypto</a>
         <a className="text-indigo1">Swift</a>
       </div>
