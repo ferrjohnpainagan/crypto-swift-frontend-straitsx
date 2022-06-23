bab5500782ce02f4a6cea792b85ebdbf43747a48
ferrjohnpainagan <ferrjohnpainagan@gmail.com>
2022-06-23 12:28:36 +0800

    Updates behavior of header buttons
    Adds wallet button
    
    Updates styling of header buttons

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,22 +1,34 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useLocation } from 'react-router-dom'
 import Remit from './Remit'
+import Wallet from './Wallet'
 
 const Header = () => {
   const navigate = useNavigate()
-  const handleHome = () => {
-    navigate('/bank-login')
+  const location = useLocation()
+  const isActivePage = (page: string) => {
+    return location.pathname.includes(page)
   }
+  const handleRemit = () => {
+    navigate('/remit/bank-login')
+  }
+  const handleWallet = () => {
+    navigate('/wallet/home')
+  }
+  // const handleHome = () => {
+  //   navigate('/remit/bank-login')
+  // }
   return (
     <div className="flex h-20 w-screen items-center border-b-gray-400 bg-white px-24 shadow-sm">
       <div
         className="cursor-pointer font-poppins text-xl font-extrabold"
-        onClick={handleHome}
+        // onClick={handleHome}
       >
         <a className="text-blue1">Crypto</a>
         <a className="text-indigo1">Swift</a>
       </div>
-      <Remit />
+      <Remit isActivePage={isActivePage} handleRemit={handleRemit} />
+      <Wallet isActivePage={isActivePage} handleWallet={handleWallet} />
     </div>
   )
 }
