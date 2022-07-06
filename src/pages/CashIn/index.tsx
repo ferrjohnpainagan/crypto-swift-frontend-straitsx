ff17765d0d939c0bc7c9bcf44c6a4651d951d018
ferrjohnpainagan <ferrjohnpainagan@gmail.com>
2022-07-06 14:33:06 +0800

    Added validation on cash in page

--- src/pages/CashIn/index.tsx
+++ src/pages/CashIn/index.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import { useNavigate, useLocation } from 'react-router-dom'
 import { useSelector } from 'react-redux'
+import { useForm, Controller } from 'react-hook-form'
+import { ErrorMessage } from '@hookform/error-message'
 import { useXaveAPI } from 'hooks/useXaveAPI'
 
 import Dropdown from 'components/Dropdown'
@@ -8,11 +10,11 @@ import Card from 'components/Card'
 import { CURRENCIES } from 'constants/index'
 import Loader from 'components/Loader'
 import Status from 'components/Status'
+import NumberFormat from 'react-number-format'
 
 const CashIn = () => {
   const navigate = useNavigate()
   const location = useLocation()
-  // const { isLoggedIn } = useSelector((state: any) => state.account)
   const isLoggedIn = localStorage.getItem('isLoggedIn')
   const accountNumber = localStorage.getItem('accountNumber')
   const customerId = localStorage.getItem('customerId')
@@ -29,21 +31,35 @@ const CashIn = () => {
 
   const { processCashIn } = useXaveAPI()
 
-  const handleCashIn = async () => {
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm({
+    criteriaMode: 'all',
+  })
+
+  const onSubmit = async (data) => {
+    console.log(data)
+    await handleCashIn(data)
+  }
+
+  const handleCashIn = async (data) => {
     setLoading(true)
+    setAmount(data.amount)
     try {
       const response = await processCashIn({
         username: username,
         customerId: customerId,
         bankAccountNumber: accountNumber,
-        amount: amount,
+        amount: data.amount,
       })
 
       if (response.status === 200) {
         setStatus('success')
         setTimeout(() => {
           navigate('/remit/success/cash-in', {
-            state: { amount: amount, currency: currency },
+            state: { amount: data.amount, currency: currency },
           })
         }, 1500)
       }
@@ -59,50 +75,82 @@ const CashIn = () => {
       <div className="flex h-16 w-full items-center border-b pl-8">
         <p className="font-workSans text-blue1">Cash-in</p>
       </div>
-
-      <div className="flex items-center justify-between px-8 py-6">
-        <div>
-          <Dropdown
-            name={'Select'}
-            options={CURRENCIES}
-            selected={currency}
-            setSelected={setCurrency}
-          />
-        </div>
-        <div style={{ width: '18vw' }}>
-          <div className="text-md font-workSans font-semibold text-black1">
-            Amount
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="flex items-center justify-between px-8 py-6">
+          <div className="h-16">
+            <Dropdown
+              name={'Select'}
+              options={CURRENCIES}
+              selected={currency}
+              setSelected={setCurrency}
+            />
           </div>
-          <div className="flex justify-between rounded-lg bg-vanilla1 p-3 align-bottom">
-            <div className="flex flex-col">
-              <div className="font-workSans text-xs">BALANCE: 200 SGD </div>
-              <input
-                type="text"
-                className="align-end mt-4 flex w-full border-none border-transparent bg-transparent font-roboto text-2xl focus:outline-none"
-                value={amount}
-                onChange={(e) => setAmount(e.target.value)}
+          <div style={{ width: '18vw' }}>
+            <div className="text-md font-workSans font-semibold text-black1">
+              Amount
+            </div>
+            <div className="flex justify-between rounded-lg bg-vanilla1 p-3 align-bottom">
+              <div className="flex flex-col">
+                <div className="font-workSans text-xs">BALANCE: 200 SGD </div>
+                <Controller
+                  control={control}
+                  name="amount"
+                  rules={{
+                    required: 'Amount is required.',
+                    pattern: {
+                      value: /^[0-9.]*$/,
+                      message: 'Amount cannot be a negative value.',
+                    },
+                  }}
+                  render={({ field }) => (
+                    <NumberFormat
+                      {...field}
+                      decimalScale={2}
+                      className="align-end mt-4 flex w-full border-none border-transparent bg-transparent font-roboto text-2xl focus:outline-none"
+                    />
+                  )}
+                />
+              </div>
+              <div className="flex self-end">
+                <button className="rounded-xl border border-blue1 p-1 text-xs text-blue1">
+                  MAX
+                </button>
+              </div>
+            </div>
+            <div className="mt-2 h-8">
+              <ErrorMessage
+                errors={errors}
+                name="amount"
+                render={({ messages }) => {
+                  console.log('messages', messages)
+                  return messages
+                    ? Object.entries(messages).map(([type, message]) => (
+                        <p
+                          className="font-workSans text-xs text-red-600"
+                          key={type}
+                        >
+                          {message}
+                        </p>
+                      ))
+                    : null
+                }}
               />
             </div>
-            <div className="flex self-end">
-              <button className="rounded-xl border border-blue1 p-1 text-xs text-blue1">
-                MAX
-              </button>
-            </div>
+
+            <button
+              // type="button"
+              type="submit"
+              className={`mt-2 flex h-12 w-full justify-center rounded-lg ${
+                !loading ? 'bg-blue1 hover:bg-blue2' : 'bg-gray1'
+              }  py-3 font-workSans font-medium text-white`}
+              disabled={loading}
+            >
+              {loading ? <Loader /> : 'Cash-in'}
+            </button>
           </div>
-          <button
-            type="button"
-            className={`mt-6 flex h-12 w-full justify-center rounded-lg ${
-              (parseFloat(amount) > 0 && currency !== '') || loading
-                ? 'bg-blue1 hover:bg-blue2'
-                : 'bg-gray1'
-            }  py-3 font-workSans font-medium text-white`}
-            disabled={!(parseFloat(amount) > 0 && currency !== '') || loading}
-            onClick={handleCashIn}
-          >
-            {loading ? <Loader /> : 'Cash-in'}
-          </button>
         </div>
-      </div>
+      </form>
+
       <div className="px-8 font-workSans text-black1">
         <div className="flex justify-between">
           <div className="text-xs">Deposited amount</div>
